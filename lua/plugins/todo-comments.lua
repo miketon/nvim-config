90635b8c1883f0b3d5cd37028617f168c61b323c
Mike Ton <mike.ton@gmail.com>
2024-10-03 11:58:26 -0500

    [todo_comments] adding ELI5 keyword entry

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/todo-comments.nvim",
+  opts = {
+    merge_keywords = true,
+    keywords = {
+      -- explain like I'm 5
+      ELI5 = {
+        icon = "👶", -- 󰹼 or 👶 if an emoji is more preferable
+        color = "info",
+        alt = { "IAM5" },
+      },
+    },
+  },
+}
